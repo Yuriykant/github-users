ea0d825485c2533e0d44e6b459b7feb288ac4804
Yuriy <ykantakuzin@bk.ru>
2024-01-17 20:48:52 +0300

    update api tokken

--- src/api/gitHub.ts
+++ src/api/gitHub.ts
@@ -1,6 +1,6 @@
 export const GITHUB_API = {
   headers: {
     Accept: 'application/json',
-    Authorization: `Bearer ${'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'}`,
+    Authorization: `Bearer ${'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'}`,
   },
 };
